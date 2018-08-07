5f2169599a6842f64e8e51efd4b7ef7f75f98ce1
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-08-06 19:56:43 -0500

    Adding views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <h1>{{ message }}</h1>
+    <div v-for="beer in products">
+      <h2>{{ beer.name }}</h2>
+      <h4>{{ beer.description }}</h4>
+      <h4>${{ beer.price }}0</h4>
+      <p>{{ beer.supplier.name }}</p>
+      <img v-bind:src="beer.images[0]" alt="No Image" width="350">
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Microbrewery Menu",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/beers").then(
+      function(response) {
+        console.log(response);
+        this.products = response.data.beers;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
 </script>
